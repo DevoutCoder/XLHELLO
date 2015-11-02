9ab5a56977a6c0d668b891a63fa8e696184f0f20
huizhubo <hui@itcast.com>
2015-11-02 17:39:20 +0800

    初始化项目

---
+++ XLHELLO/XLHELLO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XLHELLO
+//
+//  Created by apple on 15/11/2.
+//  Copyright © 2015年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
